a4f84b9907ef0d17150139aad77ba64a4df8afb6
Chase Maxwell Hennion <chase.m.hennion@seagate.com>
2017-10-12 22:59:29 -0500

    Updating mini-project.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # java-coursera
+
+This repo contains code created during Duke University's [Programming Foundations with JavaScript, HTML and CSS Course](https://www.coursera.org/learn/duke-programming-web/home/welcome)
+
+## Author
+Chase Hennion  
+<chase.m.hennion@outlook.com>
--- programming-foundations-javascript/image-filters-on-the-web/mini-project.js
+++ programming-foundations-javascript/image-filters-on-the-web/mini-project.js
@@ -1,5 +1,6 @@
 var img = null;
 var modifiedImg = null;
+var fileInput = null;
 
 function upload( canvasId, fileInputId ) {
   // Get the file input
@@ -7,28 +8,44 @@ function upload( canvasId, fileInputId ) {
   // Get the canvas
   // Draw the image onto the canvas
 
-  var fileInput = document.getElementById( fileInputId );
+  fileInput = document.getElementById( fileInputId );
   var canvas = document.getElementById( canvasId );
 
   img = new SimpleImage( fileInput );
   modifiedImg = new SimpleImage( fileInput );
   img.drawTo( canvas );
 
-  // Update the div to display the dimensions of the image
-
-  while( !img.complete() ) {}
-
-  var dimensionsDiv = document.getElementById( "imageDimensions" );
-  dimensionsDiv.innerHTML = "<h4>" + img.getWidth() + " x " + img.getHeight() + "</h4>";
+  if( isImageReady( img ) ) {
+    displayDimensions()
+  }
+  else {
+    // Wait a second before trying to get the dimensions because the image
+    // isn't ready yet
+    setTimeout( displayDimensions, 1000 );
+  }
 
 }
 
 function doGrayscale( canvasId ) {
+  var canvas = document.getElementById( canvasId );
 
+  for( var pixel of modifiedImg.values() ) {
+    setGray( pixel );
+  }
+
+  clearCanvas( canvasId );
+  modifiedImg.drawTo( canvas );
 }
 
-function doRedScale( canvasId ) {
+function doRedscale( canvasId ) {
+  var canvas = document.getElementById( canvasId );
 
+  for( var pixel of modifiedImg.values() ) {
+    pixel.setRed( 255 );
+  }
+
+  clearCanvas( canvasId );
+  modifiedImg.drawTo( canvas );
 }
 
 function doRainbow( canvasId ) {
@@ -38,4 +55,36 @@ function doRainbow( canvasId ) {
 function doReset( canvasId ) {
   // Draw the original image back on the canvas.
   // Reset the modified image
+  var canvas = document.getElementById( canvasId );
+  clearCanvas( canvasId );
+  img.drawTo( canvas );
+  modifiedImg = new SimpleImage( fileInput );
+}
+
+function displayDimensions() {
+
+  var dimensionsDiv = document.getElementById( "imageDimensions" );
+  dimensionsDiv.innerHTML = "<h4>" + img.getWidth() + " x " + img.getHeight() + "</h4>";
+
+}
+
+function isImageReady( image ) {
+  if( image == null || !image.complete() ) {
+    return false;
+  }
+  return true;
+}
+
+function setGray( pixel ) {
+  var avg = ( pixel.getRed() + pixel.getGreen() + pixel.getBlue() ) / 3;
+  pixel.setRed( avg );
+  pixel.setGreen( avg );
+  pixel.setBlue( avg );
+  return pixel;
+}
+
+function clearCanvas( canvasId ) {
+  var canvas = document.getElementById( canvasId );
+  var context = canvas.getContext( "2d" );
+  context.clearRect( 0, 0, canvas.width, canvas.height );
 }
